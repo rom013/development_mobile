15db98ec19c28b7a41dfbdb2f18facb8fd4ac54c
rom013 <romullomelo0130@gmail.com>
2023-08-28 15:35:38 -0300

    feat: start atv_list02_function

---
+++ atv_dart_02_functions/prog_09.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+main() {
+	try {
+		stdout.write("Nota da primeira nota: ");
+		String test1 = stdin.readLineSync()!;
+		stdout.write("Nota da segunda nota: ");
+		String test2 = stdin.readLineSync()!;
+		stdout.write("Nota da terceira nota: ");
+		String test3 = stdin.readLineSync()!;
+		
+
+
+	} catch (e) {
+		print("Valor invalido");
+	}
+}
+
+
+
+calcTestestAverage(double a, double b, {double c = 0}){
+    
+}
