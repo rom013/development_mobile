1affe7aa189b50ea322e291c828242a5c15f5989
rom013 <romullomelo0130@gmail.com>
2023-09-14 12:14:43 -0300

    atv class

---
+++ atv_dart_03_class/prog_01.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+class Calculator{
+  double firstNumber = 0;
+  double secondNumber = 0;
+
+  Calculator(this.firstNumber, this.secondNumber);
+
+  calculate(int operator){
+    if(operator == 1){
+      return firstNumber + secondNumber;
+    }
+    else if(operator == 2){
+      return firstNumber - secondNumber;
+    }
+    else if(operator == 3){
+      return firstNumber * secondNumber;
+    }
+    else if(operator == 4){
+      if(secondNumber == 0){
+        print("Não é possível fazer divisão por zero");
+        return;
+      }
+      return firstNumber / secondNumber;
+    }
+  }
+}
+
+
+main(){
+  try {
+    stdout.write("Primeiro número: ");
+    double firstNumber = double.parse(stdin.readLineSync()!);
+    stdout.write("Segundo número: ");
+    double secondNumber = double.parse(stdin.readLineSync()!);
+    print("Operador matemático: \n soma: 1 \n subtração: 2 \n multiplicação: 3 \n divisão: 4");
+    int operatorValue = int.parse(stdin.readLineSync()!);
+
+
+    Calculator calculate = Calculator(firstNumber, secondNumber);
+    print(calculate.calculate(operatorValue));
+  } catch (e) {
+    print("o valor inserido é invalido");
+  }
+}
---
+++ atv_dart_03_class/prog_03.dart
@@ -0,0 +1,15 @@
+class Contato {
+  String name;
+  int phoneNumber;
+  String addresEmail;
+
+  Contato(this.addresEmail, this.name, this.phoneNumber);
+}
+
+class LivrosDeEnderecos {
+  
+}
+
+main(){
+  
+}
